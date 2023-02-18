1f7b8584173208bbeb35a8176045a9008b92c7a1
Albert Álef <85764913+albertalef@users.noreply.github.com>
2023-02-18 07:47:56 -0300

    chore: add big decimal support to calculate start bid

--- src/renderer/src/api/OpenSeaApiHandler.ts
+++ src/renderer/src/api/OpenSeaApiHandler.ts
@@ -1,12 +1,13 @@
 import { SlugData } from "@renderer/interfaces/AutoBidInterfaces";
 import axios from "axios";
+import bigDecimal from "js-big-decimal";
 
 const instance = axios.create({
     headers: { "Accept": "application/json", "X-API-KEY": "d0b6281e87d84702b020419fdf58ea81", },
     baseURL: "https://api.opensea.io",
 });
 
-
+const etherConstant = new bigDecimal(1e18);
 
 export default class OpenSeaApiHandler {
 
@@ -19,14 +20,13 @@ export default class OpenSeaApiHandler {
     }
 
     static async getStartBid(slug: string): Promise<number>{
-        const etherConstant = (10) ** -18;
 
         const url = `v2/offers/collection/${slug}?order_by=eth_price&order_direction=asc`;
         const { data } = await instance.get(url);
         
-        const value = (data.offers[0].protocol_data.parameters.offer[0].startAmount) * etherConstant;
-        return Number((value + 0.0001).toFixed(4))
-
+        const value = new bigDecimal(data.offers[0].protocol_data.parameters.offer[0].startAmount);
+        console.log(data);
+        return Number(value.divide(etherConstant, 5).add(new bigDecimal('0.00001')).getValue())
     }
 
     static async getSlugData(slug: string): Promise<SlugData>{
